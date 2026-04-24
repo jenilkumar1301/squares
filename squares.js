8f66dcb9dd28f0a98c28ce3c7dd2b5340fa0aa0f
jenilkumar1301 <sarojpaghdal31@gmail.com>
2026-04-24 13:42:39 -0400

    Use addSquare function on page load

--- squares.js
+++ squares.js
@@ -3,18 +3,8 @@ let squareArea = document.querySelector("#squarearea");
 let squareCount = parseInt(Math.random() * 21) + 30;
 
 for (let i = 0; i < squareCount; i++) {
-
-    let square = document.createElement("div");
-    square.className = "square";
-
-    square.style.left = parseInt(Math.random() * 650) + "px";
-    square.style.top = parseInt(Math.random() * 250) + "px";
-
-    square.style.backgroundColor = randomColor();
-
-    squareArea.append(square);
+    addSquare();
 }
-
 
 
 function addSquare() {
